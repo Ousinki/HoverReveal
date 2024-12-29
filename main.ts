5415df63dfacd282117e319d5b4191ba2c20d9d5
Asrieal-4000k <asrieal@qq.com>
2024-12-30 01:24:37 +0800

    feat：删除调试样式

--- main.ts
+++ main.ts
@@ -25,13 +25,15 @@ export default class MyPlugin extends Plugin {
 		console.log('HoverReveal plugin loading...');
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon 
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// // This creates an icon in the left ribbon.
+		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		// 	// Called when the user clicks the icon.
+		// 	new Notice('This is a notice!');
+		// });
+
+		
+		// // Perform additional things with the ribbon 
+		// ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
